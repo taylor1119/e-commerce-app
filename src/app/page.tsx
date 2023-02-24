41e8f49a9077c06905427561c42423a5cdace887
taylor1119 <taylorcarter1119@gmail.com>
2023-02-24 14:50:47 +0100

    Added products guide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import FeaturedProductsCarousel from '@/components/FeaturedProductsCarousel';
+import ProductsGuide from '@/components/ProductsGuide';
 
 export default function Home() {
 	return (
 		<main>
 			<FeaturedProductsCarousel />
+			<ProductsGuide />
 		</main>
 	);
 }
