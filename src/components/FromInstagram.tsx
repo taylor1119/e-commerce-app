06529cf545ecc7659a4ee08c58aad350cf4988bd
taylor1119 <taylorcarter1119@gmail.com>
2023-02-24 16:18:10 +0100

    Centered instagram photos group on large screens

--- src/components/FromInstagram.tsx
+++ src/components/FromInstagram.tsx
@@ -13,9 +13,9 @@ export default function FromInstagram() {
 	return (
 		<section>
 			<h1 className='pt-14 text-center font-secondary text-4xl'>Popular Instagram photos</h1>
-			<ul className='flex snap-x snap-mandatory overflow-x-auto py-14'>
+			<ul className='flex snap-x snap-mandatory justify-start overflow-x-auto py-14 xl:justify-center'>
 				{INSTAGRAM_PHOTOS.map((photo, index) => (
-					<li className='group relative shrink-0 snap-center lg:shrink' key={index}>
+					<li className='group relative shrink-0 snap-center xl:shrink' key={index}>
 						<Image width={320} height={320} alt='instagram photo' src={photo} quality={100} />
 						<div className='absolute bottom-5 z-20 flex w-full translate-y-5 justify-center gap-4 text-lg opacity-0 duration-300 group-hover:translate-y-0 group-hover:opacity-100'>
 							<Link
