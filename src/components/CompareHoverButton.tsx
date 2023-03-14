e638fae03205f2f66c20657977625db9b6494026
taylor1119 <taylorcarter1119@gmail.com>
2023-03-14 01:12:18 +0100

    Removed ui elements related to a user account

--- src/components/CompareHoverButton.tsx
+++ src/components/CompareHoverButton.tsx
@@ -4,7 +4,6 @@ import { compareItemsState } from '@/recoil/atoms';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
-import Tooltip from './common/Tooltip';
 
 export default function CompareHoverButton() {
 	const [hasCompareItems, setHasCompareItems] = useState(false);
@@ -13,16 +12,16 @@ export default function CompareHoverButton() {
 
 	if (!hasCompareItems) return null;
 	return (
-		<Tooltip title='Compare' postilion='top'>
+		<div className='fixed bottom-2 left-2 hidden sm:max-md:block'>
 			<Link
 				href='/compare'
-				className='group fixed bottom-2 left-2 hidden h-10 w-10 items-center justify-center rounded-full bg-teal-400 text-xl text-white sm:flex'
+				className='flex h-10 w-10 items-center justify-center rounded-full bg-teal-400 text-xl text-white'
 			>
 				<i className='ri-arrow-left-right-fill' />
 			</Link>
 			<span className='fixed bottom-8 left-9 flex h-5 w-5 items-center justify-center rounded-full bg-red-400 text-xs font-semibold text-white'>
 				{compareItemsNumber}
 			</span>
-		</Tooltip>
+		</div>
 	);
 }
--- src/components/Header/HeaderLeft.tsx
+++ src/components/Header/HeaderLeft.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { sidebarOpenState, wishlistItemsState } from '@/recoil/atoms';
+import { compareItemsState, sidebarOpenState, wishlistItemsState } from '@/recoil/atoms';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
@@ -8,22 +8,34 @@ import { useRecoilValue, useSetRecoilState } from 'recoil';
 export default function HeaderLeft() {
 	const setSidebarOpen = useSetRecoilState(sidebarOpenState);
 	const openSidebar = () => setSidebarOpen(true);
+
 	const wishlistItemsNumber = useRecoilValue(wishlistItemsState).length;
 	const [hasWishlistItems, setHasWishlistItems] = useState(false);
 	useEffect(() => setHasWishlistItems(Boolean(wishlistItemsNumber)), [wishlistItemsNumber]);
+
+	const [hasCompareItems, setHasCompareItems] = useState(false);
+	const compareItemsNumber = useRecoilValue(compareItemsState).length;
+	useEffect(() => setHasCompareItems(Boolean(compareItemsNumber)), [compareItemsNumber]);
 
 	return (
 		<ul className='flex w-16 items-center justify-start gap-4 text-xl'>
 			<li className='cursor-pointer hover:opacity-50 md:hidden' onClick={openSidebar}>
 				<i className='ri-menu-line' />
 			</li>
-			<li className='hidden cursor-pointer hover:opacity-50 md:block'>
-				<i className='ri-user-line' />
+			<li className='relative hidden cursor-pointer hover:opacity-50 md:block'>
+				<Link href='/compare'>
+					{hasCompareItems && (
+						<span className='absolute right-3 bottom-4 flex h-5 w-5 items-center justify-center rounded-full bg-teal-400 text-xs font-semibold text-white'>
+							{compareItemsNumber}
+						</span>
+					)}
+					<i className='ri-arrow-left-right-fill' />
+				</Link>
 			</li>
 			<li className='relative cursor-pointer hover:opacity-50 md:block'>
 				<Link href='/wishlist'>
 					{hasWishlistItems && (
-						<span className='absolute left-3 bottom-4 flex h-5 w-5 items-center justify-center rounded-full bg-teal-400 text-xs font-semibold text-white'>
+						<span className='absolute right-3 bottom-4 flex h-5 w-5 items-center justify-center rounded-full bg-teal-400 text-xs font-semibold text-white'>
 							{wishlistItemsNumber}
 						</span>
 					)}
